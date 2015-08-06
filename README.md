dbb33c0ce70f929d1bdd88f9afcd5f017ecd9acf
Alexey Buzmakov <buzmakov@gmail.com>
2015-08-06 16:32:16 +0300

    add basic http auth and some tests

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Этот модуль получает ID эксперимента из хранилища и проводит томографическую реконструкцию этого эксперимента.
 
 Для запуска mongodb в контейнере docker 
-docker run --name my-mongo3 -p 127.0.0.1:27017:27017 -v /home/makov/workspace/tomo-storage/data/db/:/data/db -d mongo:3 --storageEngine wiredTiger
+
+docker run --name my-mongo3 -p 127.0.0.1:27017:27017 -v /home/makov/workspace/tomo_storage/data/db/:/data/db -d mongo:3 --storageEngine wiredTiger
--- storage/web_storage.py
+++ storage/web_storage.py
@@ -1,21 +1,35 @@
-from flask import Flask, url_for, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "admin": "tomo_admin",
+    "user": "tomo_user"
+}
+
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
 
 
 def get_list_of_experiments():
     return 'Get list of all experiments'
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
+@auth.login_required
 def hello_world():
-    with app.test_request_context():
-        return '\n'.join([url_for('experiment'), ])
+    return jsonify(user=auth.username())
 
 
-@app.route('/experiment', methods=['GET'])
-@app.route('/experiment/<experiment_id>', methods=['GET', 'POST'])
-def experiment(experiment_id=None):
+@app.route('/experiments', methods=['GET'])
+@app.route('/experiments/<experiment_id>', methods=['GET', 'POST'])
+def experiments(experiment_id=None):
     if experiment_id is None:
         return get_list_of_experiments()
     else:
